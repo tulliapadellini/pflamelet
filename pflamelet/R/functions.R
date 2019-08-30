7bb004fc5ed7063e61dc0e93e51f87bd8c02f128
Tullia Padellini <tullia.padellini@gmail.com>
2019-08-30 17:44:42 +0200

    started bootstrap function

--- pflamelet/R/functions.R
+++ pflamelet/R/functions.R
@@ -66,7 +66,56 @@ build.flamelet = function(X, base.type = "landscape", base.param = 1, dimension=
 
 
 
+#' Bootstrap Band for Persistence Flamelet
+#'
+#' Computes a bootstrap band for the Persistence Flamelet from a list of Persistence Diagrams. If X is a matrix or a dataframe the function computes the flamelets of the
+#' kernel density estimator computed on X.
+#'
+#'@param X   a list of persistence diagrams representing the scale-space family at different resolution.
+#'            If \code{X} is a n-by-d matrix or a data.frame containing a d-dimensional pointcloud,
+#'            this function computes the Flamelet on the corresponding KDE with the bandwidth as scale parameter.
+#'@param base.type a string specifying whether the Flamelet is built from Persistence Landscapes ("landscape") or Persistence Silhouettes ("silhouettes")
+#'@param base.param the order k of the Flamelet (if base.type=="landscape") or the power p of the Flamelet (if base.type=="silhouette")
+#'@param dimension the topological dimension of the flamelet (0 for connected components, 1 for loops, ...)
+#'@param tseq a vector of values at which the Flamelet function is evaluated for a fixed scale level
+#'@param h.grid vector of bandwidths for the KDE, representing the scale parameter of the Flamelet
+#'@param lim 2-by-d matrix, where the i-th column contains the range of the grid over which KDE is computed for the i-th variable.
+#'@param by a scalar (or a vector if different values are selected for each dimension)
+#'             specifying spaces between elements on the grid
+#'@param scale a logical indicating whether or not the Persistence Diagrams have to be scaled to be in the same range (needed only for visualization purposes)
+#'@examples
+#'
+#'## library(TDA)
+#'## xx = rbind(circleUnif(50, 1), circleUnif(50, 1.5) + 3)
+#'## Xlim = c(-1, 5);  Ylim = c(-1, 5);  by = 0.05
+#'## lim = cbind(Xlim, Ylim)
+#'## foo.flamelet = build.flamelet(X = xx, h.grid = seq(0.01, 1, length.out = 40),
+#'## base.type = "landscape", dimension = 1,base.param = 1, lim = lim, by = by,
+# ##                              tseq = seq(0, .75, length.out = 500))
+#'@export
+build.flamelet = function(X, base.type = "landscape", base.param = 1, dimension=1, tseq, h.grid =NULL, lim = NULL, by=NULL, scale = TRUE){
 
+  if(!is.list(X)){
+    f = function(h) {
+      diagr = gridDiag(X, kde, lim = lim, by = by, sublevel = FALSE,
+                       location = FALSE, printProgress = FALSE, h = h, maxdimension = dimension)$diagram
+      if(scale) diagr[,2:3] = diagr[,2:3]/diagr[1,3]
+      return(diagr)
+    }
+
+    cat("Computing the Persistence Diagrams - hold on, this is the longest step...")
+    diagListh = lapply(h.grid, f)
+    cat(" and now the Persistence Landscapes")
+
+  } else {
+    diagListh = X
+  }
+
+  if(base.type == "landscape") flamelet = sapply(diagListh, landscape, dimension = dimension, KK = base.param, tseq = tseq)
+  if(base.type == "silhouette") flamelet = sapply(diagListh, silhouette, dimension = dimension, p = base.param, tseq = tseq)
+  return(flamelet)
+
+}
 
 
 # if (plot) {
